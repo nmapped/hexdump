db1f617d3671ed41b6c8cd20f08ed4166578906c
nmapped <nmapped@arch.none>
2025-04-01 12:58:29 +0100

    add mem offset

--- main.c
+++ main.c
@@ -14,17 +14,20 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	unsigned char buffer[8];
+	unsigned char buffer[16];
 	size_t bytes_read;
+	size_t offset = 0;
 
 	/* fread(buffer, element size, no of elements, stream)
 	 -> moves file pointer forward automatically
 	 */
 	while ((bytes_read = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
+		printf("%08zX\t", offset);
 		for (size_t i = 0; i < bytes_read; i++) {
 			printf("%02X ", buffer[i]);
 		}
 		printf("\n");
+		offset += bytes_read;
 	}
 
 	fclose(fp);
