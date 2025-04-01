f79821eb3e4f226a4056cf5b39335e473582a0c9
nmapped <nmapped@arch.none>
2025-04-01 14:11:49 +0100

    cleanup

--- main.c
+++ main.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void print_hex_ln(unsigned char* buffer, size_t* bytes_read, size_t* offset) {
-	printf("%08zX\t", *offset);
-	for (size_t i = 0; i < *bytes_read; i++) {
-		printf("%02X ", buffer[i]);
-		if (i % 16 == 15 || i == *bytes_read - 1) {
-			printf("\t");
-			for (size_t j = i - (i % 16); j <= i; j++) {
-				printf("%c", isprint(buffer[j]) ? buffer[j] : '.');
-			}
-			printf("\n");
-		}
-	}
+void print_hex_ln(unsigned char* buffer, size_t bytes_read, size_t* offset) {
+    printf("%08zX  ", *offset);  // print offset
+
+    // print hex values
+    for (size_t i = 0; i < bytes_read; i++) {
+        printf("%02X ", buffer[i]);
+    }
+
+    // print ascii representation
+    printf("\t");
+    for (size_t i = 0; i < bytes_read; i++) {
+        printf("%c", isprint(buffer[i]) ? buffer[i] : '.');
+    }
+
+    printf("\n");
 }
 
 int main(int argc, char** argv) {
-	if (argc < 2) printf("usage: %s <filename>\n", argv[0]);
+    if (argc < 2) {
+        printf("usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
-	FILE* fp = fopen(argv[1], "rb");
+    FILE* fp = fopen(argv[1], "rb");
+    if (fp == NULL) {
+        printf("error: can't open file %s\n", argv[1]);
+        return 1;
+    }
 
-	if (fp == NULL) printf("there was an error opening the file\n"); 
+    unsigned char buffer[16];
+    size_t bytes_read;
+    size_t offset = 0;
 
-	unsigned char buffer[16];
-	size_t bytes_read;
-	size_t offset = 0;
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
+        print_hex_ln(buffer, bytes_read, &offset);
+        offset += bytes_read;
+    }
 
-	while ((bytes_read = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
-		print_hex_ln(buffer, &bytes_read, &offset);
-		offset += bytes_read;
-	}
-
-	return 0;
+    fclose(fp);
+    return 0;
 }
